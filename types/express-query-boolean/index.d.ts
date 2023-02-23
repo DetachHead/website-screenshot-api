f2b33484a20fd2e8bc0ae6bde5a73d006871328a
DetachHead <detachhead@users.noreply.github.com>
2023-02-24 00:36:24 +1000

    switch to my fork of express-query-boolean with types

--- types/express-query-boolean/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'express-query-boolean' {
-    import { Request, Response, NextFunction } from 'express'
-
-    export default function parseBool(): (req: Request, res: Response, next: NextFunction) => void
-}
