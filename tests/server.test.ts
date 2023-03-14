a835e7eb68420e607fb91d424b7c8f2009084b5e
DetachHead <detachhead@users.noreply.github.com>
2023-03-14 20:40:59 +1000

    remove unnessecary async/awaits and just return the promises

--- tests/server.test.ts
+++ tests/server.test.ts
@@ -14,33 +14,21 @@ afterAll((done) => {
 
 describe('positive', () => {
     describe('html', () => {
-        test('screenshotMode=full', async () => {
-            await supertest(app).get(`/?screenshotMode=full&input=${inputHTML}`).expect(200)
-        })
-        test('screenshotMode=normal', async () => {
-            await supertest(app).get(`/?input=${inputHTML}`).expect(200)
-        })
-        test('screenshotMode=element', async () => {
-            await supertest(app).get(`/?input=${inputHTML}`).expect(200)
-        })
+        test('screenshotMode=full', () =>
+            supertest(app).get(`/?screenshotMode=full&input=${inputHTML}`).expect(200))
+        test('screenshotMode=normal', () => supertest(app).get(`/?input=${inputHTML}`).expect(200))
+        test('screenshotMode=element', () => supertest(app).get(`/?input=${inputHTML}`).expect(200))
     })
     describe('site', () => {
         // test it on itself
         const site = `localhost:${port}`
-        test('without protocol', async () => {
-            await supertest(app).get(`/?input=${site}`).expect(200)
-        })
-        test('with protocol', async () => {
-            await supertest(app).get(`/?input=http://${site}`).expect(200)
-        })
+        test('without protocol', () => supertest(app).get(`/?input=${site}`).expect(200))
+        test('with protocol', () => supertest(app).get(`/?input=http://${site}`).expect(200))
     })
 })
 
 describe('negative', () => {
-    test('invalid site', async () => {
-        await supertest(app).get('/?input=www.sdfgsdfg.asdfasdfadsgsdf').expect(500)
-    })
-    test('no input', async () => {
-        await supertest(app).get('/').expect(422)
-    })
+    test('invalid site', () =>
+        supertest(app).get('/?input=www.sdfgsdfg.asdfasdfadsgsdf').expect(500))
+    test('no input', () => supertest(app).get('/').expect(422))
 })
